2f9b863c7c4d693636c47315833df80a0b7ce0dc
Hila <hilabarkan.hb@gmail.com>
2024-12-03 11:07:02 +0200

    chatgpt sctipt works now

--- chatgpt.py
+++ chatgpt.py
@@ -1,16 +1,24 @@
 import openai
-openai.api_key = 'YOUR_API_KEY'
+from openai import OpenAI
+
+client = OpenAI(
+    # defaults to os.environ.get("OPENAI_API_KEY")
+    api_key="private",
+)
+
+
+def chat_gpt(prompt):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return response.choices[0].message.content.strip()
+
 messages = [ {"role": "system", "content": 
               "You are a intelligent assistant."} ]
 while True:
     message = input("User : ")
     if message:
-        messages.append(
-            {"role": "user", "content": message},
-        )
-        chat = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=messages
-        )
-    reply = chat.choices[0].message.content
-    print(f"ChatGPT: {reply}")
-    messages.append({"role": "assistant", "content": reply})
+        reply = chat_gpt(message)
+        print(f"ChatGPT: {reply}")
+        messages.append({"role": "assistant", "content": reply})
