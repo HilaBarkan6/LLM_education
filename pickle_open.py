2865fcb11042916dc56e45e75ba768cfcfe5065e
Hila <hilabarkan.hb@gmail.com>
2024-12-04 09:40:46 +0200

    added script that ask chatgpt a question

--- pickle_open.py
+++ pickle_open.py
@@ -1,9 +1,34 @@
 import pandas as pd
+import openai
+from openai import OpenAI
 
  # Replace 'file_path.pkl' with the path to your .pkl file
-file_path = "C:\\Projects\\LLM_education\\ds18bb_sol100(vaad).pkl"
+file_path = "C:\\Projects\\LLM_education\\translated_questions_dataset.pkl"
 
 df = pd.read_pickle(file_path)
 
-print("Dataframe Loaded Successfully:")
-print(df.head())  # Display the first few rows
+q16a_2018_aa = df.loc[(df['exam_year'] == 2018) & (df['semester'] == "A") & (df['moed'] == "A") & (df['question_number'] == "16-a") ]
+msg = q16a_2018_aa.iloc[0]['question_translation_latex']
+
+
+with open("api_key.txt", "r") as api_key_file:
+    key = api_key_file.readline()
+
+client = OpenAI(
+    api_key=key,
+)
+
+
+def chat_gpt(prompt):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return response.choices[0].message.content.strip()
+
+reply = chat_gpt(msg)
+print(f"ChatGPT: {reply}")
+
+with open("reply.txt", "w") as output_file:
+    output_file.write(reply)
+
