7acf5a7b7659ff8205b58259fee8726a631e225f
Hila <hilabarkan.hb@gmail.com>
2024-12-11 16:20:33 +0200

    added results for llm as a judge expirement

--- pickle_open.py
+++ pickle_open.py
@@ -7,7 +7,7 @@ file_path = "C:\\Projects\\LLM_education\\translated_questions_dataset.pkl"
 
 df = pd.read_pickle(file_path)
 
-current_question = df.loc[(df['exam_year'] == 2018) & (df['semester'] == "A") & (df['moed'] == "A") & (df['question_number'] == "16-b") ]
+current_question = df.loc[(df['exam_year'] == 2018) & (df['semester'] == "A") & (df['moed'] == "A") & (df['question_number'] == "13-a") ]
 msg = current_question.iloc[0]['question_translation_latex']
 real_solution = current_question.iloc[0]['answer_translation_latex']
 
@@ -32,9 +32,9 @@ msg_prefix = "Write the solution to the following question in latex format: "
 reply = chat_gpt(msg_prefix + msg)
 print(f"ChatGPT: {reply}")
 
-with open("reply_16b.txt", "w") as output_file:
+with open("reply_13a.txt", "w") as output_file:
     output_file.write(reply)
 
-with open("ptbs_answer_16b.txt", "w") as ptbs_file:
+with open("ptbs_answer_13a.txt", "w") as ptbs_file:
     ptbs_file.write(real_solution)
 
