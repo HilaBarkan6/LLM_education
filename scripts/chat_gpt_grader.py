1830bea6357717310f1c9525e1bd0a42c704e4a1
Hila <hilabarkan.hb@gmail.com>
2025-01-06 23:34:15 +0200

    added final data and some scripts

--- scripts/chat_gpt_grader.py
+++ scripts/chat_gpt_grader.py
@@ -19,26 +19,31 @@ def chat_gpt(prompt):
     )
     return response.choices[0].message.content.strip()
 
-df = pd.read_csv("C:\\Projects\\LLM_education\\results\\questions_A_with_chat_solutions_checked.csv")
-current_questions = df.loc[(df['is_chat_correct'] != "question") & (df['is_ptbs_good'] != "no")]
+current_questions = pd.read_csv("C:\\Projects\\LLM_education\\finaldata\\finaldata.csv")
 grade_results = []
 
-for index, row in current_questions.iterrows():
-    real_solution = row['answer_translation_latex']
-    chat_solution = row['chat_answer_new']
-    msg = f"You are a computer science professor at the university. \
-     You will be given two solutions to a question from a test. \
-     The first is the teacher solution and it is correct for sure, The second is a student solution that needs to be graded.\
-     Please check the student solution. Give it a grade between 1 and 4, where 1 is for a very incorrect answer and 4 is for a great answer.\
-     You are only allowed to answer a number that is 1 or 2 or 3 or 4, write just the grade without any additional text.\
-     \n Teacher solution: {real_solution} \n student solution: {chat_solution}"
-    
-    chat_grade_response = chat_gpt(msg)
 
-    print(row['question_number'])
-    print(chat_grade_response)
-    grade_results.append(chat_grade_response)
+for i in range(5):
+    for index, row in current_questions.iterrows():
+        if(row['is_chat_correct'] == "question" or row['is_ptbs_good'] == "no"):
+            grade_results.append("")
+        else:
+            real_solution = row['answer_translation_latex']
+            chat_solution = row['chat_answer']
+            msg = f"You are a computer science professor at the university. \
+            You will be given two solutions to a question from a test. \
+            The first is the teacher solution and it is correct for sure, The second is a student solution that needs to be graded.\
+            Please check the student solution. Give it a grade between 1 and 10, where 1 is for a very incorrect answer and 10 is for a great answer.\
+            You are only allowed to answer a number between 1 and 10, write just the grade without any additional text.\
+            \n Teacher solution: {real_solution} \n student solution: {chat_solution}"
+            
+            chat_grade_response = chat_gpt(msg)
 
+            print(index)
+            grade_results.append(chat_grade_response)
 
-current_questions['chat_grade'] = grade_results
-current_questions.to_csv("C:\\Projects\\LLM_education\\results\\questionsA_chatgpt_grader_output.csv")
+    current_questions[f'chatgpt_grader_10_results_iter_{i+1}'] = grade_results
+    grade_results = []
+    print(f"iteration {i+1} completed")
+
+current_questions.to_csv("C:\\Projects\\LLM_education\\results\\finalgradings10.csv")
